e88326ba5d7a5ab7ad7a73f2821540130ba3ede0
tanusamaksimova <tatsiana.maks.98@gmail.com>
2026-07-29 20:52:11 +0300

    CssSelectors

---
+++ src/main/java/start/SelectorsXpath.java
@@ -0,0 +1,28 @@
+package start;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SelectorsXpath {
+
+    WebDriver driver = new ChromeDriver();
+    @Test
+    public void phoneBookTests() {
+        driver.get("https://telranedu.web.app/home");
+        driver.manage().window().maximize();
+        pause(2000);
+
+        driver.quit();
+
+    }
+
+    public void pause(int time){
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
